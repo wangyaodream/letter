68b44b7c46e19dfbfa9584693c7f14f63996e970
wangyao <wangyao.dream@hotmail.com>
2023-01-08 21:46:24 +0800

    add comment on the detail page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
 from .forms import EmailPostForm
+from .forms import CommentForm
 # Create your views here.
 
 
@@ -32,9 +33,29 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # save to database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 def post_share(request, post_id):
@@ -45,6 +66,7 @@ def post_share(request, post_id):
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
+
         if form.is_valid():
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
@@ -59,7 +81,8 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    })
 
 # class-based views
 
